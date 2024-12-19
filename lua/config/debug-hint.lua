7ded8de692ac7d35ae93c2161abc8c522dabc426
ac-manjaro <2419280542@qq.com>
2024-12-20 01:35:09 +0800

    add some keymap to dap and add a hint window for keymap

---
+++ lua/config/debug-hint.lua
@@ -0,0 +1,49 @@
+local function set_keymap(mode, key, cmd, desc)
+    vim.api.nvim_set_keymap(mode, key, cmd, { noremap = true, silent = true, desc = desc })
+end
+
+local function show_keymap_hints()
+    local buf = vim.api.nvim_create_buf(false, true)
+    local keymap_hints = {
+        "Keymap Hints:",
+        "<F2>  - Breakpoint Condition",
+        "<F3>  - Toggle Breakpoint",
+        "<F4>  - Run/Continue",
+        "<F5>  - Run with Args",
+        "<F6>  - Run to Cursor",
+        "<S-F6> - Go to Line (No Execute)",
+        "<F7>  - Step Into",
+        "<F8>  - Step Out",
+        "<S-F8> - Step Over",
+        "<F9>  - Run Last",
+        "<F10> - Pause",
+    }
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, keymap_hints)
+    local width = 40
+    local height = #keymap_hints
+    local opts = {
+        style = "minimal",
+        relative = "editor",
+        width = width,
+        height = height,
+        row = (vim.o.lines - height) / 2,
+        col = (vim.o.columns - width) / 2,
+        border = "rounded", -- Add a rounded border
+        noautocmd = true, -- Prevent triggering autocmds
+    }
+    local win = vim.api.nvim_open_win(buf, true, opts)
+
+    -- Autocmd to close the window when it loses focus
+    vim.api.nvim_create_autocmd("WinLeave", {
+        buffer = buf,
+        callback = function()
+            vim.api.nvim_win_close(win, true)
+        end,
+    })
+end
+
+-- Command to show keymap hints
+vim.api.nvim_create_user_command("Dbh", show_keymap_hints, {})
+
+-- Keymap to trigger the command
+set_keymap("n", "<leader>dh", ":Dbh<CR>", "Show DAP Keymap Hints")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,23 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+-- Helper function to set keymaps
+local function set_keymap(mode, key, cmd, desc)
+    vim.api.nvim_set_keymap(mode, key, cmd, { noremap = true, silent = true, desc = desc })
+end
+
+-- set dap keymaps
+set_keymap("n", "<F2>", "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", "Breakpoint Condition")
+set_keymap("n", "<F3>", "<cmd>lua require('dap').toggle_breakpoint()<CR>", "Toggle Breakpoint")
+set_keymap("n", "<F4>", "<cmd>lua require('dap').continue()<CR>", "Run/Continue")
+set_keymap("n", "<F5>", "<cmd>lua require('dap').continue({ before = get_args })<CR>", "Run with Args")
+set_keymap("n", "<F6>", "<cmd>lua require('dap').run_to_cursor()<CR>", "Run to Cursor")
+set_keymap("n", "<S-F6>", "<cmd>lua require('dap').goto_()<CR>", "Go to Line (No Execute)")
+set_keymap("n", "<F7>", "<cmd>lua require('dap').step_into()<CR>", "Step Into")
+set_keymap("n", "<S-F8>", "<cmd>lua require('dap').step_out()<CR>", "Step Out")
+set_keymap("n", "<F8>", "<cmd>lua require('dap').step_over()<CR>", "Step Over")
+set_keymap("n", "<F9>", "<cmd>lua require('dap').run_last()<CR>", "Run Last")
+set_keymap("n", "<F10>", "<cmd>lua require('dap').pause()<CR>", "Pause")
+require("config.debug-hint")
