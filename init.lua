e1d3cc56de1f7c23a95b63bede89e3f49d63d16b
ac-manjaro <2419280542@qq.com>
2024-12-19 17:42:06 +0800

    some changes

--- init.lua
+++ init.lua
@@ -2,4 +2,12 @@
 require("config.lazy")
 
 vim.o.wrap = true
-vim.o.scrolloff = 5
+vim.o.scrolloff = 10
+
+-- set a 4 spaces tab
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.autoindent = true
+vim.o.smartindent = true
