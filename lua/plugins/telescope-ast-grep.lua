8fb1264a4c2c099cc20f6d1b7c5f798b12d22299
ac-manjaro <2419280542@qq.com>
2024-12-28 20:34:23 +0800

    install telesope-sg and config

--- lua/plugins/telescope-ast-grep.lua
+++ lua/plugins/telescope-ast-grep.lua
@@ -1,23 +1,33 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-        {
-            "Marskey/telescope-sg",
-            config = function()
-                require("telescope").setup({
-                    extensions = {
-                        ast_grep = {
-                            command = {
-                                "sg",
-                                "--json=stream",
-                            }, -- must have --json=stream
-                            grep_open_files = false, -- search in opened files
-                            lang = nil, -- string value, specify language for ast-grep `nil` for default
-                        },
-                    },
-                })
-                require("telescope").load_extension("ast_grep")
-            end,
+
+    { "Marskey/telescope-sg" },
+
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
         },
+        opts = function(_, opts)
+            local telescope = require("telescope")
+            telescope.setup(vim.tbl_deep_extend("force", opts, {
+                extensions = {
+                    ast_grep = {
+                        command = {
+                            "sg",
+                            "--json=stream",
+                        }, -- must have --json=stream
+                        grep_open_files = false, -- search in opened files
+                        lang = nil, -- string value, specify language for ast-grep `nil` for default
+                    },
+                },
+            }))
+        end,
+        config = function(_, opts)
+            require("telescope").load_extension("ast_grep")
+            -- Key mapping for <leader>sA
+            vim.keymap.set("n", "<leader>sA", function()
+                require("telescope").extensions.ast_grep.ast_grep()
+            end, { desc = "Telescope AST Grep" })
+        end,
     },
 }
